51a76d75973e09d92df013bb6319d7afce3a8875
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-04 18:27:16 -0500

    Created initial function in atm.js

---
+++ atm.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const account = require('./account.js');
+
+function getBalance(){
+
+};
+
+function withdraw(){
+
+};
+
+function deposit(){
+
+};
+
+function validatePin(){
+
+};
+
+module.exports.balance = getBalance;
+module.exports.withdraw = withdraw;
+module.exports.deposit = deposit;
+module.exports.validatePin = validatePin;
